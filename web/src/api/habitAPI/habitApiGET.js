26e7c8b0af24ba84d11fdaa385d28c8f6c87de11
Nick Monacelli <nick.monacelli@ire.minnstate.edu>
2020-02-09 14:13:49 -0600

    Added mapping/render of habit_goals within the server API to the web home route

--- web/src/api/habitAPI/habitApiGET.js
+++ web/src/api/habitAPI/habitApiGET.js
@@ -3,6 +3,6 @@ import habit from "./instance";
 
 export const getHabitGoals = async () => {
     const result = await habit("habit_goal/");
-    return result;
+    return result.data.data;
 };
 
--- web/src/layouts/Home.js
+++ web/src/layouts/Home.js
@@ -1,31 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import {getHabitGoals}  from "../api/habitAPI/habitApiGET";
-
+import React, { useState, useEffect } from "react";
+import { getHabitGoals } from "../api/habitAPI/habitApiGET";
 
 const Home = () => {
-    const [habits, setHabits] = useState(null);
- 
-    useEffect(() => {
-        if(habits) {
-            return; 
-        }
-    // Step 1: Utilize axios to fetch habit-goals from the api 
+  const [habits, setHabits] = useState(null);
+
+  useEffect(() => {
+    if (habits) {
+      return;
+    }
+    // Step 1: Utilize axios to fetch habit-goals from the api
     // Step 2: fetched data set to the local state using useState hook
-    // Currently I am getting an empty return from the API due to CORS preflight. 
-      getHabitGoals().then(setHabits)
+    getHabitGoals().then(setHabits);
+  }, [habits]);
 
-    }, [habits]);
-    console.log(habits);
-    // Step 3: Return statement that has two cases
-    // Step 3 - Case A: There is no state, return loading..
+  // Step 3: Helper function for conditionally rendered Home.js
+  const renderContent = () => {
+    // Step 3 - Case A: If there is no state, return loading..
+    if (!habits) {
+      return <div>Loading...</div>;
+    }
     // Step 3 - Case B: If there is state, map over the habit objects creating a habit
-    // component for each. 
-    return (
-        <div>
-            <p>Coming from home!</p>
-
-        </div>
-    );
-}
+    // component for each.
+    return habits.map(habit => (
+      <li class="item" key={habit.id}>
+        {habit.name}
+      </li>
+    ));
+  }
+  // The Home.js return statement, which renders the renderContent helper
+  return <div className="ui divided list">{renderContent()}</div>;
+};
 
 export default Home;
