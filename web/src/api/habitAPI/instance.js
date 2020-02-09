7c1436601db30387c50085e37a55f247ac0a4398
Nick Monacelli <nick.monacelli@ire.minnstate.edu>
2020-02-09 12:40:04 -0600

    Working fetch from Web to Server

--- web/src/api/habitAPI/instance.js
+++ web/src/api/habitAPI/instance.js
@@ -4,8 +4,6 @@ export default axios.create({
   baseURL: "http://localhost:5000/",
   method: 'get',
   headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
+   
   }
 });
