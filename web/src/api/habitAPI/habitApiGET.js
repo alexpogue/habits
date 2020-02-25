029ad25b7167325f4895255f4922f8b7155667c7
Nick Monacelli <nick.monacelli@ire.minnstate.edu>
2020-02-24 18:47:29 -0600

    changed API requests from habit_goal to goal/

--- web/src/api/habitAPI/habitApiGET.js
+++ web/src/api/habitAPI/habitApiGET.js
@@ -3,17 +3,17 @@ import habit from "./instance";
 
 
 export const getAllHabitGoals = async () => {
-    const result = await habit("habit_goal/");
+    const result = await habit("goal/");
     return result.data.data;
 };
 
 export const getHabitGoal = async (data) => {
-    const result = await habit("habit/goal/" + data);
+    const result = await habit("goal/" + data);
     return result.data.data;
 }
 
 export const postHabitGoal = async (data) => {
-    await habit.post("habit_goal/", {name: data});
-    const newGoalArray =  await habit("habit_goal/");
+    await habit.post("goal/", {name: data});
+    const newGoalArray =  await habit("goal/");
     return newGoalArray.data.data;
 }
