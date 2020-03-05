540b3be1b831cdb73b226f70a397c4943a8dc66b
Nick Monacelli <nick.monacelli@ire.minnstate.edu>
2020-03-05 11:04:39 -0600

    added update and delete functionality for goals, transitioned forms to modals

--- web/src/api/habitAPI/goalApiGet.js
+++ web/src/api/habitAPI/goalApiGet.js
@@ -1,20 +1,32 @@
-import habit from "./instance";
+import goal from "./instance";
 
 // Gets all goals from the goal api.
-export const getAllHabitGoals = async () => {
-  const result = await habit("goal/");
+export const getAllGoals = async () => {
+  const result = await goal("goal/");
   return result.data.data;
 };
 
 // Gets a goal with matching goalId if invoked with a goalId.
-export const getHabitGoal = async goalId => {
-  const result = await habit("goal/" + goalId);
+export const getGoal = async goalId => {
+  const result = await goal(`goal/${goalId}`);
   return result.data.data;
 };
 
 // Posts a single goal to the goalAPI, returns with the new goal array.
-export const postHabitGoal = async data => {
-  await habit.post("goal/", { name: data });
-  const newGoalArray = await habit("goal/");
+export const postGoal = async name => {
+  await goal.post("goal/", { name: name });
+  const newGoalArray = await goal("goal/");
+  return newGoalArray.data.data;
+};
+
+export const putGoal = async (newName, id) => {
+  await goal.put(`goal/${id}`, { name: newName });
+  const newGoalArray = await goal("goal/");
+  return newGoalArray.data.data;
+};
+
+export const deleteGoal = async (id) => {
+  await goal.delete(`goal/${id}`);
+  const newGoalArray = await goal("goal/");
   return newGoalArray.data.data;
 };
--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -1,28 +1,39 @@
-import React, {useState} from 'react';
-
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Form = props => {
-  // component state
-  const [fieldInfo, setFieldInfo] = useState([]);
 
+  //  ---------State-----------
 
-  // the handleChange callback responsible for rendering inputted form text back to the user.
+  const [fieldInfo, setFieldInfo] = useState(null);
+
+  //  ---------Helper Methods-----------
+
+  //Callback responsible for rendering inputted form text back to the user.
   const handleChange = e => {
     setFieldInfo(e.target.value);
   };
 
-  // the handleSubmit callback is invoked with formData, and passes it to the parent via the parents onSubmit callback.
+  //Callback is invoked with formData, and passes it to the parent via the parents onSubmit callback.
   const handleSubmit = e => {
     props.onSubmit(fieldInfo);
     e.preventDefault();
   };
 
+//  ---------Form Return-----------
+
   return (
-    <form onSubmit = {handleSubmit}>
-        <input onChange={handleChange} placeholder="Input Goal" value={fieldInfo}></input>
-        <button type="submit">Submit</button>
-      </form>
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={handleChange}
+        placeholder={props.placeholder}
+        value={fieldInfo}
+      />
+      <button className={props.buttonClass} type="submit">
+        {props.buttonTitle}
+      </button>
+    </form>
   );
-}
+};
 
 export default Form;
