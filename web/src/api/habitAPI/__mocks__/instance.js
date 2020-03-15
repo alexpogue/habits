3e41936c79ff09260c850fc7819665c40fd29eb4
Alex Pogue <alex@epogue.com>
2020-03-15 10:52:37 -0500

    Fix tests and add test for getting specific goal

--- web/src/api/habitAPI/__mocks__/instance.js
+++ web/src/api/habitAPI/__mocks__/instance.js
@@ -1,15 +1,24 @@
 'use strict';
 
-const instance = jest.genMockFromModule('./instance');
+const instance = jest.genMockFromModule('axios');
 
 let mockGoals = Object.create(null);
 function __setMockGoals(newMockGoals) {
     mockGoals = newMockGoals;
 }
 
+function isNumeric(possibleNum) {
+    return !isNaN(possibleNum);
+}
+
 function get(path) {
-    if (path == 'goal/') {
-        return Promise.resolve({'data': mockGoals});
+    if (path === 'goal/') {
+        return Promise.resolve({'data': {'data': mockGoals}});
+    }
+    let pathSplit = path.split('/');
+    if (pathSplit[0] === 'goal' && isNumeric(pathSplit[1])) {
+        let goalNum = parseInt(pathSplit[1]);
+        return Promise.resolve({'data': {'data': mockGoals[goalNum - 1]}});
     }
 }
 
--- web/src/api/habitAPI/goalApiGet.test.js
+++ web/src/api/habitAPI/goalApiGet.test.js
@@ -1,10 +1,8 @@
 'use strict';
 
-import habit from './instance';
-
 jest.mock('./instance');
 
-describe('fetchAllGoals', () => {
+describe('fetchGoals', () => {
     const MOCKED_GOALS = [
         {'id': 1, 'name': 'Eat nachos'},
         {'id': 2, 'name': 'Do dishes'},
@@ -15,11 +13,28 @@ describe('fetchAllGoals', () => {
         require('./instance').__setMockGoals(MOCKED_GOALS);
     });
 
-    test('fetches all goals', () => {
+    test('fetches all goals', done => {
         const goalApiGet = require('./goalApiGet');
         goalApiGet.getAllHabitGoals()
             .then(values => {
                 expect(values).toEqual(MOCKED_GOALS);
+                done();
+            })
+            .catch(err => {
+                done(err);
+            });
+    });
+
+    test('fetches single goal', done => {
+        const goalApiGet = require('./goalApiGet');
+        debugger;
+        goalApiGet.getHabitGoal(1)
+            .then(value => {
+                expect(value).toEqual(MOCKED_GOALS[0]);
+                done();
+            })
+            .catch(err => {
+                done(err);
             });
     });
 });
